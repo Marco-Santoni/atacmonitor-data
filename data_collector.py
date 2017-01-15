03cab6911b21cbd6b306e91051a26da9b044477b
Marco Santoni <marcosantoni@hotmail.it>
2017-01-15 13:32:23 +0100

    Add two new paline

--- data_collector.py
+++ data_collector.py
@@ -111,23 +111,24 @@ s2 = Server('http://muovi.roma.it/ws/xml/paline/7')
 token = s1.autenticazione.Accedi(DEV_KEY, '')
 
 while True:
-    timestamp = datetime.datetime.utcnow()
-    res = s2.paline.Previsioni(token, '70638', 'it')
-    risposta = res['risposta']
-    arr = Arrival(risposta['nome'], risposta['collocazione'])
-    arr.created_at = timestamp
-    for p in risposta['primi_per_palina']:
-        for a in p['arrivi']:
-            arr.linea = a.get('linea')
-            arr.tempo_attesa = a.get('tempo_attesa')
-            arr.tempo_attesa_secondi = a.get('tempo_attesa_secondi')
-            arr.distanza_fermate = a.get('distanza_fermate')
-            arr.id_palina = a.get('id_palina')
-            arr.nome_palina = a.get('nome_palina')
-            arr.collocazione = a.get('collocazione')
-            arr.capolinea = a.get('capolinea')
-            arr.in_arrivo = bool(a.get('in_arrivo'))
-            arr.a_capolinea = bool(a.get('a_capolinea'))
+    for palina in ['70638', '72074', '70200']
+        timestamp = datetime.datetime.utcnow()
+        res = s2.paline.Previsioni(token, palina, 'it')
+        risposta = res['risposta']
+        arr = Arrival(risposta['nome'], risposta['collocazione'])
+        arr.created_at = timestamp
+        for p in risposta['primi_per_palina']:
+            for a in p['arrivi']:
+                arr.linea = a.get('linea')
+                arr.tempo_attesa = a.get('tempo_attesa')
+                arr.tempo_attesa_secondi = a.get('tempo_attesa_secondi')
+                arr.distanza_fermate = a.get('distanza_fermate')
+                arr.id_palina = a.get('id_palina')
+                arr.nome_palina = a.get('nome_palina')
+                arr.collocazione = a.get('collocazione')
+                arr.capolinea = a.get('capolinea')
+                arr.in_arrivo = bool(a.get('in_arrivo'))
+                arr.a_capolinea = bool(a.get('a_capolinea'))
 
-            save(arr)
+                save(arr)
     time.sleep(int(os.environ.get('PULL_FREQUENCY')))

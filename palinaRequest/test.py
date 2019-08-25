ee2a618f692e482018dcb1a63e8bdbf07b83c429
Ubuntu <ubuntu@ip-172-31-36-130.eu-central-1.compute.internal>
2019-08-25 18:26:13 +0000

    test missing palina

--- palinaRequest/test.py
+++ palinaRequest/test.py
@@ -1,5 +1,5 @@
 import unittest
-from xmlrpc.client import Server
+from xmlrpc.client import Server, Fault
 from unittest.mock import patch
 import os
 
@@ -9,10 +9,10 @@ class TestPalinaRequest(unittest.TestCase):
  
     def setUp(self):
         s1 = Server(os.environ['ATAC_AUTH_URL'], use_builtin_types=True)
-        token = s1.autenticazione.Accedi(os.environ['ATAC_DEV_KEY'], os.environ['ATAC_ID_UTENTE'])
+        self.token = s1.autenticazione.Accedi(os.environ['ATAC_DEV_KEY'], os.environ['ATAC_ID_UTENTE'])
         self.event = {
             'body': {
-                'token': token,
+                'token': self.token,
                 'id_palina': '10037'
             }
         }
@@ -23,6 +23,17 @@ class TestPalinaRequest(unittest.TestCase):
         lambda_handler(self.event, self.context)
         res = mock_arrival_process.call_args[0][0]
         assert('id_richiesta' in res)
+
+    @patch('arrival.process', autospec=True)
+    def test_palina_not_exists(self, mock_arrival_process):
+        missing_palina = {
+            'body': {
+                'token': self.token,
+                'id_palina': 'FOO_PALINA'
+            }
+        }
+        with self.assertRaises(Fault):
+            lambda_handler(missing_palina, self.context)
 
 if __name__ == '__main__':
     unittest.main()
